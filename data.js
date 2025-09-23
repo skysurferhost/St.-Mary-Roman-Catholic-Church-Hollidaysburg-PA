d1baba2680a7421da47b069682b8bd50d2a76843
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-09-22 23:31:54 -0400

    360

---
+++ data.js
@@ -0,0 +1,575 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-st-mary-roman-catholic-church-hollidaysburg-pa",
+      "name": "St. Mary Roman Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4827073997623792,
+          "pitch": 0.07655792477946122,
+          "rotation": 0,
+          "target": "2-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.3926647464045967,
+          "pitch": -0.9387870844938639,
+          "rotation": 0,
+          "target": "3-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.2910084942877589,
+          "pitch": 1.0652600414105926,
+          "rotation": 0,
+          "target": "1-st-mary-roman-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-st-mary-roman-catholic-church-hollidaysburg-pa",
+      "name": "St. Mary Roman Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1325968277829173,
+          "pitch": -0.9169222594508675,
+          "rotation": 0,
+          "target": "0-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.4915445155899487,
+          "pitch": -1.0718044234629893,
+          "rotation": 0,
+          "target": "3-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.6021792136634883,
+          "pitch": -0.23754148571837597,
+          "rotation": 0,
+          "target": "2-st-mary-roman-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-st-mary-roman-catholic-church-hollidaysburg-pa",
+      "name": "St. Mary Roman Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.24097302192836878,
+          "pitch": -1.5164182925713803,
+          "rotation": 0,
+          "target": "3-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.004203843800176799,
+          "pitch": 0.169945255044464,
+          "rotation": 0,
+          "target": "4-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -1.5740489585878752,
+          "pitch": 0.20430484904419544,
+          "rotation": 0,
+          "target": "1-st-mary-roman-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-st-mary-roman-catholic-church-hollidaysburg-pa",
+      "name": "St. Mary Roman Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5506169419678777,
+          "pitch": 0.9825322664974632,
+          "rotation": 0,
+          "target": "0-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.04350374175762184,
+          "pitch": 1.5053054488605682,
+          "rotation": 0,
+          "target": "2-st-mary-roman-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-st-mary-roman-catholic-church-hollidaysburg-pa",
+      "name": "St. Mary Roman Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.010516387573376917,
+          "pitch": 0.2110241522031746,
+          "rotation": 0,
+          "target": "5-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 3.1203424967628175,
+          "pitch": 0.03827703331005772,
+          "rotation": 0,
+          "target": "2-st-mary-roman-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-st-mary-roman-catholic-church-hollidaysburg-pa",
+      "name": "St. Mary Roman Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.004379056491961464,
+          "pitch": 0.3763008127116336,
+          "rotation": 0,
+          "target": "6-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -3.1210085675801427,
+          "pitch": 0.21138966529101388,
+          "rotation": 0,
+          "target": "4-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.10106252932304649,
+          "pitch": -1.2950351945583165,
+          "rotation": 0,
+          "target": "7-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.004000116317442348,
+          "pitch": 0.06606750367279801,
+          "rotation": 0,
+          "target": "8-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.005362408334480762,
+          "pitch": -0.341902377930392,
+          "rotation": 0,
+          "target": "9-st-mary-roman-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-st-mary-roman-catholic-church-hollidaysburg-pa",
+      "name": "St. Mary Roman Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1394073513718217,
+          "pitch": 0.5619617426310413,
+          "rotation": 0,
+          "target": "5-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 3.0856921258651404,
+          "pitch": -1.2847089763303643,
+          "rotation": 0,
+          "target": "7-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 3.1395167013708303,
+          "pitch": 0.20990626704247006,
+          "rotation": 0,
+          "target": "4-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.006661635999975601,
+          "pitch": 0.11933890354831789,
+          "rotation": 0,
+          "target": "8-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.004441126853475197,
+          "pitch": -0.34924360466784776,
+          "rotation": 0,
+          "target": "9-st-mary-roman-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-st-mary-roman-catholic-church-hollidaysburg-pa",
+      "name": "St. Mary Roman Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.02700185007243583,
+          "pitch": 1.0985791803198666,
+          "rotation": 0,
+          "target": "6-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 3.1367071460708864,
+          "pitch": 1.0375557932954766,
+          "rotation": 0,
+          "target": "5-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.0022205611048899954,
+          "pitch": 0.21215428386314628,
+          "rotation": 0,
+          "target": "8-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 2.6929924956675677e-10,
+          "pitch": -0.21639521788343963,
+          "rotation": 0,
+          "target": "9-st-mary-roman-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-st-mary-roman-catholic-church-hollidaysburg-pa",
+      "name": "St. Mary Roman Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.012541345269752924,
+          "pitch": -0.2913454018550645,
+          "rotation": 0,
+          "target": "7-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.015089495833914057,
+          "pitch": 0.1908227705887704,
+          "rotation": 0,
+          "target": "6-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.011330518328877659,
+          "pitch": -0.9788688073808682,
+          "rotation": 0,
+          "target": "9-st-mary-roman-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-st-mary-roman-catholic-church-hollidaysburg-pa",
+      "name": "St. Mary Roman Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.002273967433325197,
+          "pitch": 0.4878238307500151,
+          "rotation": 0,
+          "target": "6-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 3.096139759116223,
+          "pitch": 1.2683477196078528,
+          "rotation": 0,
+          "target": "8-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.003404038424461575,
+          "pitch": 0.23759366080397726,
+          "rotation": 0,
+          "target": "5-st-mary-roman-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.0022205760059783586,
+          "pitch": -0.2290679347784348,
+          "rotation": 0,
+          "target": "7-st-mary-roman-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
